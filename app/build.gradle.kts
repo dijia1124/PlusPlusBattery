fca054241e55e0e06e5690f4fe099eb61028ec2e
dijia1124 <zrh19981123@gmail.com>
2025-04-24 22:30:02 +1000

    Feature: root-related batt info

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.datastore.preferences)
+    implementation(libs.core)
     ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
