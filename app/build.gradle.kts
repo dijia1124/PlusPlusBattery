585652f72ce8d71efb28757ced54c7043e473608
dijia1124 <zrh19981123@gmail.com>
2025-05-03 23:17:16 +1000

    Bump up version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.plusplusbattery"
         minSdk = 31
         targetSdk = 35
-        versionCode = 5
-        versionName = "2.1.2"
+        versionCode = 6
+        versionName = "2.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
