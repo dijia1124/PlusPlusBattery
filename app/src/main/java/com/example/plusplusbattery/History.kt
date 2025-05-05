2e574a169d9271b2113aa8b73d4848daea662aa8
dijia1124 <zrh19981123@gmail.com>
2025-05-06 02:03:13 +1000

    Use Material 3 top app bar

--- app/src/main/java/com/example/plusplusbattery/History.kt
+++ app/src/main/java/com/example/plusplusbattery/History.kt
@@ -7,16 +7,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
-import androidx.compose.material.TopAppBar
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -30,39 +30,48 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun History(historyInfoViewModel: HistoryInfoViewModel) {
     Scaffold(
         topBar = {
-            TopAppBar(
-                backgroundColor = MaterialTheme.colorScheme.surface,
-            ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(56.dp),
-
-                    contentAlignment = Alignment.Center
-                ) {
+            androidx.compose.material3.TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.surface
+                ),
+                title = {
                     Text(
-                        text = stringResource(R.string.history),
+                        text = stringResource(R.string.app_name),
                         style = MaterialTheme.typography.headlineSmall
                     )
-                }
-            }
+                },
+            )
         },
         content = { innerPadding ->
-            Column(modifier = Modifier
-                .padding(innerPadding)
-                .background(MaterialTheme.colorScheme.background)) {
+            Column(modifier = Modifier.padding(innerPadding)) {
                 Box(
                     modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center,
                 ) {
-                    HistoryInfoScreen(historyInfoViewModel)
+                    HistoryContent(historyInfoViewModel)
                 }
             }
         }
     )
+}
+
+@Composable
+fun HistoryContent(historyInfoViewModel: HistoryInfoViewModel) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+    )
+    {
+        Column() {
+            HistoryInfoScreen(historyInfoViewModel)
+        }
+    }
 }
 
 @Composable
