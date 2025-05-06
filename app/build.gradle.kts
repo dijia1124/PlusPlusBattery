048a55e9f27685cfedea5d25b7919a9651352828
dijia1124 <zrh19981123@gmail.com>
2025-05-06 21:40:11 +1000

    Fix: coroutine issue for calibMultiplier

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.plusplusbattery"
         minSdk = 31
         targetSdk = 35
-        versionCode = 11
-        versionName = "2.1.8"
+        versionCode = 12
+        versionName = "2.1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
