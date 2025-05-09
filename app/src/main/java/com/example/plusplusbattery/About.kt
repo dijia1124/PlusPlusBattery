a7e66d592f080d7033cd066a77f205d865664eed
dijia1124 <zrh19981123@gmail.com>
2025-05-09 22:52:10 +1000

    Add version name in About screen

--- app/src/main/java/com/example/plusplusbattery/About.kt
+++ app/src/main/java/com/example/plusplusbattery/About.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.plusplusbattery.ui.components.AppScaffold
@@ -24,12 +25,16 @@ fun About(currentTitle: String) {
 
 @Composable
 fun AboutScreen() {
+    val context = LocalContext.current
+    val packageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
+    val versionName = packageInfo.versionName
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 16.dp)
             .verticalScroll(rememberScrollState())
     ) {
+        Text(stringResource(R.string.version, versionName.toString()))
         Text(stringResource(R.string.instructions_content).trimIndent())
     }
 }
