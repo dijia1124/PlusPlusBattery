77381cf9d71edcc3d29b5b480c3ff0e1293b093c
dijia1124 <zrh19981123@gmail.com>
2025-05-09 22:52:10 +1000

    Add version name in About screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.plusplusbattery"
         minSdk = 31
         targetSdk = 35
-        versionCode = 12
-        versionName = "2.1.9"
+        versionCode = 13
+        versionName = "2.1.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
