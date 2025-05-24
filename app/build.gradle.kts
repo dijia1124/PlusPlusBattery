078f4f68bfc0a9853715ab874e94802f159be20b
dijia1124 <zrh19981123@gmail.com>
2025-05-24 22:10:58 +1000

    chore: bump version code to 18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.plusplusbattery"
         minSdk = 31
         targetSdk = 35
-        versionCode = 17
-        versionName = "2.2.2"
+        versionCode = 18
+        versionName = "2.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
