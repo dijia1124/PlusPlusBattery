5d61fec7fe55e71867dc4f56e6170a1b288ddad1
dijia1124 <zrh19981123@gmail.com>
2025-06-11 15:14:58 +0800

    chore: bump version code to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.dijia1124.plusplusbattery"
         minSdk = 31
         targetSdk = 35
-        versionCode = 20
-        versionName = "2.2.5"
+        versionCode = 21
+        versionName = "2.2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
