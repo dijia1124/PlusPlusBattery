8c2470316bdde3e23c3826e65c7db0d8c0ca749b
dijia1124 <zrh19981123@gmail.com>
2025-07-16 20:28:27 +0800

    chore: bump version code to 22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.dijia1124.plusplusbattery"
         minSdk = 31
         targetSdk = 35
-        versionCode = 21
-        versionName = "2.2.6"
+        versionCode = 22
+        versionName = "2.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
