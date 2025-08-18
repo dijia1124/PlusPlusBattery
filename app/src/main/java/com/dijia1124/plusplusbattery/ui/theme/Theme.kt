6dca6da1240ebdc93636da2f6cd03142553f4a5f
dijia1124 <zrh19981123@gmail.com>
2025-08-19 02:27:19 +1000

    fix(theme): change status bar color accordingly when dark mode is manually toggled in app settings

--- app/src/main/java/com/dijia1124/plusplusbattery/ui/theme/Theme.kt
+++ app/src/main/java/com/dijia1124/plusplusbattery/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.dijia1124.plusplusbattery.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,11 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = RedLightPrimary,
@@ -47,6 +52,18 @@ fun PlusPlusBatteryTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    // Status bar color
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat
+                .getInsetsController(window, view)
+                .isAppearanceLightStatusBars = !darkTheme
+        }
     }
 
     MaterialTheme(
