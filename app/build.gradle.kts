308e33e0ffba76315030300e28d31bf43e109d72
Runhui Zhou <34638934+dijia1124@users.noreply.github.com>
2025-09-13 02:10:16 +1000

    ci: sonarqube code quality & chore: bump version code to 25 (#20)
    * chore: bump version code to 25
    
    * ci: set up ci-based SonarQube code analysis
    
    * chore(strings): set formatted to false for instructions_content & unknown_due_to_fcc_equals_design_capacity
    
    * chore(strings): add back missing escape characters for en string resource

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.dijia1124.plusplusbattery"
         minSdk = 31
         targetSdk = 35
-        versionCode = 24
-        versionName = "2.3.2"
+        versionCode = 25
+        versionName = "2.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,12 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
+    id("org.sonarqube") version "6.3.1.5724"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "dijia1124_PlusPlusBattery")
+        property("sonar.organization", "te08")
+    }
 }
